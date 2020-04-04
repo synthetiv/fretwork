b011d7d3405f36fabfe97774d6779f7829e1ed9a
Kenji Garland <kenjigarland@gmail.com>
2020-04-05 04:30:53 +0700

    Double-click any memory type or voice to select all

--- lib/grid_multi_select.lua
+++ lib/grid_multi_select.lua
@@ -8,6 +8,14 @@ function MultiSelect.new(x, y, width, height)
 	local instance = setmetatable(Select.new(x, y, width, height), MultiSelect)
 	instance.held = {}
 	instance.selected = {}
+	instance.awaiting_double_click = false
+	instance.double_click_metro = metro.init{
+		time = 0.2,
+		event = function()
+			instance.awaiting_double_click = false
+		end,
+		count = 1
+	}
 	instance:reset()
 	return instance
 end
@@ -49,17 +57,21 @@ function MultiSelect:is_selected(option)
 	return self.selected[option]
 end
 
--- TODO: double-press any key to select all
 function MultiSelect:key(x, y, z)
 	if not self:should_handle_key(x, y) then
 		return
 	end
 	local option = self:get_key_option(x, y)
 	if z == 1 then
-		if not self:is_held() then
+		if self:is_selected(option) and self.awaiting_double_click then
+			self:reset(true)
+			return
+		elseif not self:is_held() then
 			self:reset()
 		end
 		self.selected[option] = true
+		self.awaiting_double_click = true
+		self.double_click_metro:start()
 	end
 	self.held[option] = z == 1
 end
